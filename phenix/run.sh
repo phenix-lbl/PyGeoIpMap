f58360d612cf603d8617fe61138f9bf9cd1d60a1
Billy K. Poon <bkpoon@lbl.gov>
2025-08-20 15:34:39 -0700

    Update for running on cci2

--- phenix/run.sh
+++ phenix/run.sh
@@ -1,2 +1,2 @@
 # python ../pygeoipmap.py -i ./unique_ip.txt -o ./world.png --service m --db ./GeoLite2-City.mmdb >& log
-python ../pygeoipmap.py -i ./unique_ip.txt  -o ./usa.png --service m --db ./GeoLite2-City_20250228/GeoLite2-City.mmdb --extents=-130/-65/23/50 >& log
+python ../pygeoipmap.py -i ./unique_ip.txt  -o ../../phenixwebsite_metrics/figures/fig_usa.png --service m --db ./GeoLite2-City_20250819/GeoLite2-City.mmdb --extents=-130/-65/23/50 >& log
